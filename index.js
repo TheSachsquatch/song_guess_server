4cef656b16f23dc9156d547dee839175c020e9e6
TheSachsquatch <vsachin@umich.edu>
2022-06-23 00:48:49 -0500

    show dialog

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use((req,res, next)=>{
         req.session.access_token = "";
         req.session.refresh_token = "";
         req.session.initialized = true;
-        req.session.showDialog= false;
+        req.session.showDialog= true;
         req.session.save();
         console.log(req.session.access_token)
     }
