2816d1592e727a582efa6cc59b7dcd97742db065
TheSachsquatch <vsachin@umich.edu>
2022-06-23 00:35:18 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const sqlite = require("better-sqlite3");
+
+const SqLiteStore = require("better-sqlite3-session-store")(session)
+const db = new sqlite("sessions.db", {});
+
+const port = process.env.PORT || 5000
+
+dotenv.config()
+
+const app = express();
+
+app.use(cookieParser());
+app.use(session({secret: process.env.SESSION_SECRET,
+    store: new SqLiteStore({
+        client:db,
+        expires:{
+            clear:true,
+            intervalMs: 2592000000
+        }
+    }),
+    resave: true, saveUninitialized: true, cookie: {
+        maxAge: 2592000000, path: "/", secure: false,
+    }}));
+app.use(cors({
+    origin: ["http://localhost:3000", "https://"],
+    methods: ["GET", "HEAD",  "POST", "PUT", "DELETE"],
+    credentials: true
+}))
+
+app.use((req,res, next)=>{
+    if(!req.session.initialized){
+        req.session.access_token = "";
+        req.session.refresh_token = "";
+        req.session.initialized = true;
+        req.session.showDialog= false;
+        req.session.save();
+        console.log(req.session.access_token)
+    }
+    next();
+})
+
+app.use(require("./routes/auth"))
+
+app.listen(port, () =>{
+    console.log(`Server started at ${port}`)
+})
+
+
---
+++ routes/auth.js
@@ -0,0 +1,123 @@
+const express = require("express")
+
+const auth = express.Router();
+
+const request = require('request')
+
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+
+var generateRandomString = function (length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+    for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+};
+
+function refreshTok (req){
+    let options = {
+        url: 'https://accounts.spotify.com/api/token',
+        headers: {
+            'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64')),
+            'Content-Type' : 'application/x-www-form-urlencoded'
+        },
+        client_id: client_id,
+        form: {
+          grant_type: 'refresh_token',
+          refresh_token: req.session.refresh_token
+        },
+        json: true
+    };
+
+    request.post(options, function(error, response, body){
+        if(!error && response.statusCode===200){
+            console.log(body)
+            req.session.access_token = body.access_token;
+            req.session.save();
+            if(req.session.timer!==-1){
+                setTimeout(() => refreshTok(req), req.session.timer);
+            }
+        }
+    })
+}
+
+auth.route('/auth/login').get((req,res)=>{
+    var scope = "streaming \
+               user-read-email \
+               user-read-private \
+               user-modify-playback-state \
+               user-read-playback-state \
+               playlist-read-collaborative \
+               playlist-read-private" 
+    var state = generateRandomString(16);
+    console.log(req.session);
+    var auth_query_parameters = new URLSearchParams({
+        response_type: "code",
+        client_id: client_id,
+        scope: scope,
+        redirect_uri: "http://localhost:5000/auth/callback",
+        show_dialog: req.session.showDialog,
+        state: state
+    })
+
+    res.redirect('https://accounts.spotify.com/authorize/?' + auth_query_parameters.toString());
+})
+
+auth.route('/auth/callback').get((req, res)=>{
+    var code = req.query.code;
+
+    var authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        form: {
+        code: code,
+        redirect_uri: "http://localhost:5000/auth/callback",
+        grant_type: 'authorization_code'
+        },
+        headers: {
+        'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64')),
+        'Content-Type' : 'application/x-www-form-urlencoded'
+        },
+        json: true
+    };
+
+    request.post(authOptions, function(error, response, body) {
+        if(error){
+            console.log(error)
+        }
+        if (!error && response.statusCode === 200) {
+            res.redirect('http://localhost:3000/')
+            req.session.access_token = body.access_token;
+            req.session.refresh_token = body.refresh_token;
+            console.log(req.sessionID);
+            console.log(req.session);
+            req.session.timer = 3400000;
+            setTimeout(() => refreshTok(req), 3400000);
+            req.session.save();
+        }
+    });
+})
+
+auth.route('/auth/token').get((req, res) =>{
+    console.log(req.session)
+    console.log(req.sessionID)
+    res.json(
+        {
+            access_token: req.session.access_token,
+        })
+})
+
+auth.route('/auth/logout').get((req,res) =>{
+    access_token = "";
+    req.session.access_token = "";
+    req.session.refresh_token = "";
+    req.session.showDialog = true;
+    req.session.timer = -1;
+    req.session.save();
+    console.log("logged out");
+    console.log(req.session);
+})
+
+module.exports = auth;
